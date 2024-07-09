043b6843fc4d812e50760a587276087c3cfcb5c8
Sagarcodes3 <mrsagarkatad@gmail.com>
2024-07-09 16:01:33 +0530

    Setup cloudinary

--- project/server.js
+++ project/server.js
@@ -1,5 +1,11 @@
 import app from "./app.js";
+import cloudinary from "cloudinary";
 
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 // Check Project port running
 app.listen(4000, ()=>{
     console.log(`Server Listening at port ${4000}`);
